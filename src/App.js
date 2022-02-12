418f2b15d8506eccf7d5e96b8c981a0d87501263
blangwell <b.langwell@outlook.com>
2022-02-12 01:56:19 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		let countFromStorage = localStorage.getItem("deathcount");
+		if (countFromStorage != null) {
+			setCount(+countFromStorage);
+		} else {
+			localStorage.setItem("deathcount", "0");
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("deathcount", count.toString())
+	}, [count]);
+
+	const die = () => {
+		setCount(count + 1);
+	}
+	
+	const undo = () => {
+		if (count > 0) {
+			setCount(count - 1);
+		}
+	}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<h1>Deaths: {count}</h1>
+			<button onClick={die}>Die</button>
+			<button onClick={undo}>Undo</button>
     </div>
   );
 }
